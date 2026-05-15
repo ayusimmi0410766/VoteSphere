b2b3932d8e1b420713e5897f1cbaf6db2b5b6ead
ayushi <ayushikumari6388@gmail.com>
2026-05-16 02:45:32 +0530

    fixed sequence issue

--- src/main/java/com/VoteSphereApplication.java
+++ src/main/java/com/VoteSphereApplication.java
@@ -11,54 +11,58 @@ import com.repository.CandidateRepository;
 import com.repository.UserRepository;
 
 @SpringBootApplication
-public class VoteSphereApplication implements CommandLineRunner{
+public class VoteSphereApplication implements CommandLineRunner   {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VoteSphereApplication.class, args);
 	}
-	
+
 	@Autowired
 	private UserRepository userRepo;
-	
+
 	@Autowired
 	private CandidateRepository canRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
-	
+
 		// Admin
 		User admin = new User();
-		admin.setId(1);
+
 		admin.setEmail("admin");
 		admin.setName("admin");
 		admin.setPassword("admin");
 		admin.setPhone(1234);
 		admin.setRole("ROLE_ADMIN");
 		admin.setStatus("admin");
-		userRepo.save(admin);
+
+
 		
-		// Candidates
+		// Candidate 1
 		Candidate candidate1 = new Candidate();
-		candidate1.setId(1);
-		candidate1.setCandidate("candidate1");
+
+		candidate1.setCandidate("
+
 		canRepo.save(candidate1);
-		
-		Candidate candidate2 = new Candidate();
-		candidate2.setId(2);
+
+		// Candidate 2
+		Candidate candidate2 = ne
+
 		candidate2.setCandidate("candidate2");
+
 		canRepo.save(candidate2);
-		
+
+
 		Candidate candidate3 = new Candidate();
-		candidate3.setId(3);
+
 		candidate3.setCandidate("candidate3");
-		canRepo.save(candidate3);
-		
-		Candidate candidate4 = new Candidate();
-		candidate4.setId(4);
-		candidate4.setCandidate("candidate4");
-		canRepo.save(candidate4);
-		
-	}
+
+
 	
 
+		candidate4.setCandidate("candidate4");
+
+		canRepo.save(candidate4);
+	}
 }
+	
